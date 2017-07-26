07ca314deb58014ead6cc2317e75ea18f393b0aa
nairnik <30476675+nairnik@users.noreply.github.com>
2017-07-26 12:04:31 -0700

    Update earthquakeWDC.js

--- WDC/earthquakeWDC.js
+++ WDC/earthquakeWDC.js
@@ -10,4 +10,10 @@
     };
 
     tableau.registerConnector(myConnector);
+    $(document).ready(function () {
+    $("#submitButton").click(function () {
+        tableau.connectionName = "USGS Earthquake Feed";
+        tableau.submit();
+    });
+});
 })();
